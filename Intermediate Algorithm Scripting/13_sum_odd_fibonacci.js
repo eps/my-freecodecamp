84aaa95833f8731ffa4df53cd7237201a81d4050
Herman Chan <hermchan89@gmail.com>
2017-08-08 15:30:15 -0700

    completed intermediate algorithm #13

--- Intermediate Algorithm Scripting/13_sum_odd_fibonacci.js
+++ Intermediate Algorithm Scripting/13_sum_odd_fibonacci.js
@@ -5,3 +5,40 @@ The first two numbers in the Fibonacci sequence are 1 and 1. Every additional nu
 
 For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
 */
+
+function sumFibs(num) {
+
+  var list = fibsUpTo(num);
+  var total = 0;
+
+  //get the sum of all odd numbers
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] % 2 == 1) {
+      total += list[i];
+    }
+  }
+  return total;
+}
+
+
+function fibsUpTo(num) {
+
+  if (num === 1) {
+    return [1,1];
+  }
+
+  var list = [1,1];
+
+  while(true) {
+    //get the next number
+    var next = list[list.length-1] + list[list.length-2];
+    if (next <= num) {
+      list.push(next);
+    } else {
+      return list;
+    }
+  }
+
+}
+
+fibsUpTo(4);
